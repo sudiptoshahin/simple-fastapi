392ad4f213ff75044e4a032bc01429b671ba86ba
sudiptoshahin <mdsshahin784@gmail.com>
2025-11-09 00:52:44 +0600

    alembic

--- app/main.py
+++ app/main.py
@@ -3,14 +3,32 @@ from fastapi import FastAPI, Depends
 from . import models
 from .database import engine
 from .routes import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # db connection
 models.Base.metadata.create_all(bind=engine)
 print('___Database_connected____')
 
-# 10.36.37
+11.34.40
 
 app = FastAPI()
+
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
